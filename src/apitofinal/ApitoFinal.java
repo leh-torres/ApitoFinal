1165630d0014a6245edc1069e1a474fd5d3eb569
Gabriel Rudan <gabrielrudam92@gmail.com>
2021-07-06 13:18:43 -0300

    testando

---
+++ src/apitofinal/ApitoFinal.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apitofinal;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author RonaldoMatos
+ */
+public class ApitoFinal extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/apitofinal/FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        Image image = new Image(getClass().getResourceAsStream("/apitofinal/Logotype.jpg"));
+        ImageView imageView = new ImageView(image);
+        
+        imageView.setImage(image);
+        imageView.setVisible(true);
+        imageView.setCache(true);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/apitofinal/FXMLDocumentController.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apitofinal;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author RonaldoMatos
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    private Label label;
+    @FXML
+    private Button cadastrar;
+    @FXML
+    private ImageView logo;
+    
+    Image image = new Image(getClass().getResourceAsStream("Logotype.jpg"));
+    @FXML
+    private Button entrar;
+    @FXML
+    private TextField txtsenha;
+    @FXML
+    private Label labelResultado;
+    @FXML
+    private TextField txtlogin;
+    
+    
+    
+    @FXML
+    public void displayImage(){
+        logo.setImage(image);
+    }
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("Você clicou no botão!");
+        labelResultado.setText("Entrou!\nEmail: "+ txtlogin.getText() + "\nSenha: "+ txtsenha.getText());
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
